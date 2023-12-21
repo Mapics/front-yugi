084e517a651b0af65578ebeb92ea8c469d99eb90
lenzees <tcely2003@gmail.com>
2023-12-21 12:08:24 +0100

    modification

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,35 @@ import Connexion from './components/connexion/connexion';
 import CarteInfo from './components/cartes/CarteInfo';
 import CarteEdit from './components/cartes/CarteEdit';
 import Inscription from './components/inscription/inscription';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    navigate('/');
+  };
+
   return (
-      <div className="App">
-        <header className="header">
+    <div className="App">
+      <header className="header">
         <div className="header-top">
           <h1>Yu-Gi-Oh</h1>
-          </div>
-          </header>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/connexion" element={<Connexion />} />
-          <Route path="/inscription" element={<Inscription />} />
-          <Route path="/carteInfo/:id" element={<CarteInfo />} />
-          <Route path="/carteEdit/:id" element={<CarteEdit />} />
-        </Routes>
-      </div>
+          {localStorage.getItem('token') && (
+            <button onClick={handleLogout}>Déconnexion</button>
+          )}
+        </div>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/connexion" element={<Connexion />} />
+        <Route path="/inscription" element={<Inscription />} />
+        <Route path="/carteInfo/:id" element={<CarteInfo />} />
+        <Route path="/carteEdit/:id" element={<CarteEdit />} />
+      </Routes>
+    </div>
   );
 }
 
