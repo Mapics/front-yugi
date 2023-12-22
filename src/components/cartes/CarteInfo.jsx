7399b19960de19caa10564d9f5848d51e6523b43
lenzees <tcely2003@gmail.com>
2023-12-22 10:27:54 +0100

    pull

--- src/components/cartes/CarteInfo.jsx
+++ src/components/cartes/CarteInfo.jsx
@@ -4,37 +4,48 @@ import axios from 'axios';
 import './CarteInfo.css';
 
 export default function CarteInfo() {
-        const [card, setCard] = useState(null);
-        const { id } = useParams();
-        const navigate = useNavigate();
+  // State variable to store card details
+  const [card, setCard] = useState(null);
+  // Extracting the 'id' parameter from the URL
+  const { id } = useParams();
+  // Navigation hook for programmatic navigation
+  const navigate = useNavigate();
 
+  // Fetching card details when the component mounts or when 'id' changes
   useEffect(() => {
     const fetchCardDetails = async () => {
       try {
         console.log(`Fetching card details for ID: ${id}`);
+        // Making a GET request to the server's API to fetch card details
         const res = await axios.get(`http://localhost:3001/cartes/${id}`);
         console.log('Response:', res.data);
+        // Setting the retrieved card details in the state
         setCard(res.data);
       } catch (error) {
         console.error("Error fetching card details", error);
       }
     };
 
+    // Calling the fetchCardDetails function
     fetchCardDetails();
   }, [id]);
 
+  // Handler for clicking the "Modifier la carte" button
   const handleEditClick = () => {
+    // Checking if the user is logged in
     const token = localStorage.getItem('token');
     if (!token) {
       console.error('Utilisateur non autorisé');
       return;
     }
+    // Navigating to the card editing page
     navigate(`/carteEdit/${id}`);
   };
-  
 
+  // Handler for clicking the "Supprimer la carte" button
   const handleDeleteClick = async () => {
     try {
+      // Retrieving the user token from localStorage
       const token = localStorage.getItem('token');
 
       if (!token) {
@@ -42,29 +53,33 @@ export default function CarteInfo() {
         return;
       }
 
+      // Making a DELETE request to the server's API to delete the card
       await axios.delete(`http://localhost:3001/cartes/${id}`, {
         headers: {
           Authorization: `Bearer ${token}`
         },
       });
+      // Navigating back to the home page after successful deletion
       navigate('/');
     } catch (error) {
       console.error('Erreur lors de la suppression de la carte', error);
     }
   };
 
+  // Handler for clicking the "Fermer" button
   const handleClose = () => {
+    // Navigating back to the home page
     navigate("/");
   };
 
+  // Checking if the card details are still being fetched
   if (!card) {
     return <div className="loading">Chargement...</div>;
   }
 
-  // Vérifiez si l'utilisateur est connecté
+  // Checking if the user is logged in
   const isUserLoggedIn = !!localStorage.getItem('token');
   console.log('Token:', localStorage.getItem('token'));
-
 
   return (
     <div className="card-info-container">
